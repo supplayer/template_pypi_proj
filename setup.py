91302b3ee20e58c2e3802bd2b11be4f42f441238
supplayer <x254724521@hotmail.com>
2021-04-02 17:21:53 +0800

    optimize setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import setuptools
 
+proj_name = 'proj_name'
+description = "Import or export Excel."
+install_requires = []
+author = "Supplayer"
+author_email = "x254724521@hotmail.com"
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="exec-excel",
-    author="Supplayer",
-    author_email="x254724521@hotmail.com",
-    description="Import or export Excel.",
+    name='_'.join(proj_name.split('_')),
+    author=author,
+    author_email=author_email,
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Supplayer/exec_excel.git",
+    url=f"https://github.com/{author}/{proj_name}.git",
     packages=setuptools.find_packages(exclude=('tests', '.gitignore', 'requirements.txt')),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,7 +24,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=[],
+    install_requires=install_requires,
     setup_requires=['setuptools_scm'],
     use_scm_version=True
 )
